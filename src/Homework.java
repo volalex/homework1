97708792fc11f67f8e0d766b2c0911b1cc759ae5
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-01-11 18:33:24 +0100

    Homework done!

--- src/Homework.java
+++ src/Homework.java
@@ -2,5 +2,6 @@ public class Homework {
   public static void main(String[] args) {
     int a = 2 + 2;
     System.out.println("Result: " + a);
+    System.out.println("Result 2: " + a * 10);
   }
 }
